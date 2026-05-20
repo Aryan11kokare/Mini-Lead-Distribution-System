869cfd79dfc38d69576a74460d7e1a5b83fa2586
Aryan Kokare <aryan11kokare@gmail.com>
2026-05-20 22:11:34 +0530

    Adding realtime pooling

--- frontend/app/dashboard/page.jsx
+++ frontend/app/dashboard/page.jsx
@@ -23,6 +23,13 @@ export default function DashboardPage() {
 
   useEffect(() => {
     fetchDashboard();
+
+    // REALTIME POLLING
+    const interval = setInterval(() => {
+      fetchDashboard();
+    }, 5000);
+
+    return () => clearInterval(interval);
   }, []);
 
   if (loading) {
